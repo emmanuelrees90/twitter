0202019155418a5896bc0c4ded83b62102bf0472
Emmanuel Oppong <emmanueloppong@Emmanuels-MacBook-Pro.local>
2025-10-04 18:50:04 -0400

    create sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import SidebarMenuItem from './SidebarMenuItem';
+import { HomeIcon } from '@heroicons/react/solid';
+import { HashtagIcon, BookmarkIcon, ClipboardIcon, InboxIcon, UserIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, BellIcon } from '@heroicons/react/outline';
+
+export default function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      {/* Twitter Logo */}
+
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image width="50" height="50"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl-TfMOnpB-VAG3HqfSeZmc3hyCp7vnuK4AQ&s"></Image>
+      </div>
+
+      {/* Menu */}
+    <div className="mt-4 mb-2.5 xl:items-start">
+      <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
+      <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+      <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+      <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+      <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+      <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+      <SidebarMenuItem text="Profile" Icon={UserIcon} />
+      <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+    </div>
+      
+
+      {/* Button */}
+
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+
+      {/* Mini-Profile*/}
+
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+        <img className="h-10 w-10 rounded-full xl:mr-2" src="https://img.freepik.com/free-photo/front-view-business-woman-suit_23-2148603018.jpg?semt=ais_hybrid&w=740&q=80" alt="user-img" />
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Mani Enterprise</h4>
+          <p className="text-gray-500">@codewithsahand</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+      </div>
+    </div>
+  )
+}
