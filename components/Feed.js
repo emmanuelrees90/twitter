423f93767dfd3930ad3c548c2ad817d975f965a0
Emmanuel Oppong <emmanueloppong@Emmanuels-MacBook-Pro.local>
2025-10-05 15:45:58 -0400

    add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,28 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id:"1",
+      name: "John Doe",
+      username: "@johndoe",
+      userImg:"https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+      img:"https://plus.unsplash.com/premium_photo-1673697239981-389164b7b87f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8fDB8fHww",
+      text: "nice view!",
+      timestamp: "2 hours ago"
+    },
+    {
+      id:"2",
+      name: "Alexa Johnson",
+      username: "@johndoe",
+      userImg:"https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+      img:"https://plus.unsplash.com/premium_photo-1673697239981-389164b7b87f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8fDB8fHww",
+      text: "wow!",
+      timestamp: "2 days ago"
+    },
+  ]
   return (
     <div className="xl:ml-[370px] border-l border-r xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
         <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +32,10 @@ export default function Feed() {
             </div>
         </div>
         <Input />
+        {
+          posts.map((post) => (
+            <Post key={post.id} post={post} />)
+        )}
     </div>
   )
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,16 @@
-import Image from 'next/image';
-import SidebarMenuItem from './SidebarMenuItem';
-import { HomeIcon } from '@heroicons/react/solid';
-import { HashtagIcon, BookmarkIcon, ClipboardIcon, InboxIcon, UserIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, BellIcon } from '@heroicons/react/outline';
+import Image from "next/image";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  InboxIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  BellIcon,
+} from "@heroicons/react/outline";
 
 export default function Sidebar() {
   return (
@@ -9,30 +18,39 @@ export default function Sidebar() {
       {/* Twitter Logo */}
 
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
-        <Image width="50" height="50"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl-TfMOnpB-VAG3HqfSeZmc3hyCp7vnuK4AQ&s"></Image>
+        <Image
+          width="50"
+          height="50"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl-TfMOnpB-VAG3HqfSeZmc3hyCp7vnuK4AQ&s"
+        ></Image>
       </div>
 
       {/* Menu */}
-    <div className="mt-4 mb-2.5 xl:items-start">
-      <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
-      <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-      <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-      <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-      <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
-      <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-      <SidebarMenuItem text="Profile" Icon={UserIcon} />
-      <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
-    </div>
-      
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
 
       {/* Button */}
 
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+        Tweet
+      </button>
 
       {/* Mini-Profile*/}
 
       <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img className="h-10 w-10 rounded-full xl:mr-2" src="https://img.freepik.com/free-photo/front-view-business-woman-suit_23-2148603018.jpg?semt=ais_hybrid&w=740&q=80" alt="user-img" />
+        <img
+          className="h-10 w-10 rounded-full xl:mr-2"
+          src="https://img.freepik.com/free-photo/front-view-business-woman-suit_23-2148603018.jpg?semt=ais_hybrid&w=740&q=80"
+          alt="user-img"
+        />
         <div className="leading-5 hidden xl:inline">
           <h4 className="font-bold">Mani Enterprise</h4>
           <p className="text-gray-500">@codewithsahand</p>
@@ -40,5 +58,5 @@ export default function Sidebar() {
         <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
       </div>
     </div>
-  )
+  );
 }
